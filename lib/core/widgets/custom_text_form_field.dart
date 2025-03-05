1e656b8f083d2612eff43588774aa1190a12140d
thisismostafahassan <iammostafahassan@gmail.com>
2025-03-05 12:24:47 +0200

    update text form field

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
+  final void Function(String)? onFieldSubmitted;
+  final void Function(String?)? onSaved;
+  final String? Function(String?)? validator;
+  final TextInputAction? textInputAction;
+  final FocusNode? focusNode;
   final TextEditingController? controller;
   final String? hint;
   final int? maxLines;
   const CustomTextFormField({
     super.key,
+    this.onFieldSubmitted,
+    this.onSaved,
+    this.textInputAction,
+    this.validator,
+    this.focusNode,
     this.controller,
     this.hint,
     this.maxLines,
@@ -13,7 +23,12 @@ class CustomTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onFieldSubmitted: onFieldSubmitted,
+      onSaved: onSaved,
+      validator: validator,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
       style: TextStyle(color: Colors.white),
       maxLines: maxLines,
       autocorrect: true,
